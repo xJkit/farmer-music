7187e352646a9299d8eb7cefe5d7a79bff3c1db9
xJkit <xjkit54780@gmail.com>
2018-05-29 02:08:35 +0800

    Add initial list view

--- containers/Search.tsx
+++ containers/Search.tsx
@@ -1,36 +1,61 @@
-import React from 'react';
-import { Text } from 'react-native';
+import * as React from 'react';
+
+import { FlatList, StyleSheet } from 'react-native';
 import { Flex, List } from 'antd-mobile';
-import ListItem from 'antd-mobile/lib/list/ListItem';
 
 import records from '../mocks/seachRecords.json';
 
-export default () => {
-  return (
-    <Flex
-      style={{
-        flex: 1,
-        backgroundColor: 'yellow',
-        alignItems: 'flex-start',
-        paddingTop: 24
-      }}
-    >
-      <Flex.Item>
-        <List renderHeader={() => '搜尋紀錄'}>
-          {records.map(record => (
-            <List.Item
-              key={record.id}
-              thumb={record.imgUrl}
-              arrow="horizontal"
-              multipleLine
-              onClick={() => {}}
-            >
-              {record.singer}
-              <List.Item.Brief>{record.albumName}</List.Item.Brief>
-            </List.Item>
-          ))}
+interface IRecord {
+  id: number;
+  albumName: string;
+  singer: string;
+  imgUrl: string;
+}
+
+class Search extends React.Component<{}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      dataSource: records
+    };
+  }
+
+  public render() {
+    return (
+      <Flex
+        style={{
+          alignItems: 'flex-start',
+          backgroundColor: 'yellow',
+          flex: 1,
+          paddingTop: 24
+        }}
+      >
+        <List style={styles.listStyle}>
+          <FlatList
+            data={this.state.dataSource}
+            onEndReachedThreshold={30}
+            onEndReached={info => console.log(info)}
+            renderItem={(record: IRecord) => {
+              const { albumName, singer } = record.item;
+              return (
+                <List.Item key={record.id}>
+                  {albumName}
+                  <List.Item.Brief>{singer}</List.Item.Brief>
+                </List.Item>
+              );
+            }}
+          />
         </List>
-      </Flex.Item>
-    </Flex>
-  );
-};
+      </Flex>
+    );
+  }
+}
+
+export default Search;
+
+const styles = StyleSheet.create({
+  listStyle: {
+    flex: 1,
+    height: 600
+  }
+});
